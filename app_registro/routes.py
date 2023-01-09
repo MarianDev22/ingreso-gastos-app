751108b3d272f42de0ecb538c4ef4e3739f214a2
Mariandev <marianmfalicante@gmail.com>
2023-01-09 12:26:43 +0100

    ultimo cambios templates delete, update, utilizacion de jinja

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -3,8 +3,34 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    #prueba de diccionario a vista de html
+    datos = [
+        {
+            'fecha':'18/12/22',
+            'concepto': 'Regalo de Reyes',
+            'cantidad':-275.50
+        },
+        {
+            'fecha':'19/12/22',
+            'concepto':'Cobro de trabajo',
+            'cantidad':1200
+        },
+        {
+            'fecha':'18/12/22',
+            'concepto':'Ropa de Navidad',
+            'cantidad':-355.50
+        }
+    ]
+    return render_template("index.html", pageTitle="Listas", lista=datos)
 
 @app.route("/new")
 def create():
-    return render_template("new.html")
+    return render_template("new.html", pageTitle="Alta")
+
+@app.route("/delete")
+def remove():
+    return render_template("delete.html", pageTitle="Borrar")
+
+@app.route("/update")
+def edit():
+    return render_template("update.html", pageTitle="Editar")
