0bd8196b258b023dbda017caf07a5cb4bec79608
Mariandev <marianmfalicante@gmail.com>
2023-01-11 23:03:13 +0100

    funcion borrar terminada

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -2,15 +2,19 @@ from app_registro import app
 from flask import render_template, request, redirect
 import csv
 from datetime import date, datetime
+from config import *
+import os # permite hacer acciones propias del sist op: borrar carpetas, crearlas, etc.
 
 @app.route("/")
 def index():
-    fichero =open ("data/movimientos.csv","r")
+    fichero =open (MOVIMIENTOS_FILE,"r")
     csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
     #prueba de diccionario a vista de html
     datos = [ ]
     for item in csvReader:
         datos.append(item)
+    
+    fichero.close()
         
     return render_template("index.html", pageTitle="Listas", lista=datos)
 
@@ -24,10 +28,10 @@ def create():
         if error:
             return render_template("new.html", pageTitle="Alta", typeAction= "Alta", typeButton="Guardar",msjerror=error, dataForm=request.form, pageForm="/new")
         else:
-            mifichero = open("data/movimientos.csv","a",newline='')
+            mifichero = open(MOVIMIENTOS_FILE,"a",newline='')
             lectura =csv.writer(mifichero, delimiter=',',quotechar='"')
             #crear id
-            fichero =open ("data/last_id.csv","r")
+            fichero =open (LAST_ID_FILE,"r")
                         
             registro = fichero.read()
             if registro =="":
@@ -37,7 +41,7 @@ def create():
 
             fichero.close()
 
-            ficheroG = open("data/last_id.cvs","w")
+            ficheroG = open(LAST_ID_FILE,"w")
             ficheroG.write(str(new_id))
             ficheroG.close()
             
@@ -50,25 +54,46 @@ def create():
 @app.route("/delete/<int:id>", methods=["GET","POST"])
 def remove(id):
     if request.method == "GET":
-        mifichero = open("data/movimientos.csv","r")
+        mifichero = open(MOVIMIENTOS_FILE,"r")
         lectura =csv.reader(mifichero, delimiter=',',quotechar='"')
         registro_buscado=[]
         for registro in lectura:
             if registro[0]== str(id):
                 registro_buscado= registro
         mifichero.close()
-        
+
         if len(registro_buscado)>0:
             return render_template("delete.html", pageTitle="Borrar", registros=registro_buscado,typeButton="Borrar")
         else:
             return ("/")
     else:
-        return f"aqui tendriamos que borrar registro con {id}"
+        fichero_old = open(MOVIMIENTOS_FILE,"r")
+        fichero = open(MOVIMIENTOS_NEW_FILE,"w", newline="")
+        csvReader= csv.reader(fichero_old, delimiter=',',quotechar='"')
+        csvWriter= csv.writer(fichero, delimiter=',',quotechar='"')
+        
+        for registro in csvReader:
+            if registro[0]!= str(id):
+                csvWriter.writerow(registro)
+        fichero.close()
+        fichero_old.close()
+        os.remove(MOVIMIENTOS_FILE)
+        os.rename(MOVIMIENTOS_NEW_FILE, MOVIMIENTOS_FILE)
+
+        return redirect("/")
 
 @app.route("/update/<int:id>")
 def edit(id):
-    #return render_template("update.html", pageTitle="Editar", typeAction="Actualización",typeButton="Editar", pageForm="/update")
-    return f"Se va a modificar el id {id}"
+    if request.method == "GET":
+        mifichero = open(MOVIMIENTOS_FILE,"r")
+        lectura =csv.reader(mifichero, delimiter=',',quotechar='"')
+        registro_buscado=[]
+        for registro in lectura:
+            if registro[0]== str(id):
+                registro_buscado= registro
+        mifichero.close()
+    return render_template("update.html", pageTitle="Editar", typeAction="Actualización",typeButton="Editar", pageForm="/update", registros=registro_buscado)
+    #return f"Se va a modificar el id {id}"
 
 def validateForm(requestForm):
     hoy = date.today().isoformat()
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-#MOVIMIENTOS_FILE= "data/movimientos.csv"
-#MOVIMIENTOS_NEW_FILE= "data/movimientos_new.csv"
-#LAST_ID_FILE=
+MOVIMIENTOS_FILE= "data/movimientos.csv"
+MOVIMIENTOS_NEW_FILE= "data/movimientos_new.csv"
+LAST_ID_FILE = "data/last_id.csv"
